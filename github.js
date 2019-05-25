ccfe29e9d219e2e4ee0c28ce0ae567dcaeab65a9
Jejo <jejomar@im.skubbs.com>
2019-05-25 15:43:41 +0800

    Added repos

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class GitHub {
     constructor() {
         this.client_id = 'a130e62cdc4217e1573a';
         this.client_secret = 'd82fb121d602676936e766d818f236161319beab';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
